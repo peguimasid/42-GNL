055065e517560190707721122273fa54e2e3e2d8
Guilhermo Masid <guilhermomasid@gmail.com>
2022-05-30 10:57:59 -0300

    add steps

---
+++ get_next_line.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gmasid <gmasid@student.42.rio>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/29 15:13:17 by gmasid            #+#    #+#             */
+/*   Updated: 2022/05/29 15:26:53 by gmasid           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*get_next_line(int fd)
+{
+	// 1:
+	// Write a function that reads a file description until he finds a char '\n'
+	// and concat what the function finds to the string you pass (aux_string)
+	// "aux_string + what the function finds"
+	////////////////////////////////////////////////////////////////////
+	// 2:
+	// Write a function that take the "aux_string" until the first \n
+	// and return this first part
+	////////////////////////////////////////////////////////////////////
+	// 3:
+	// Write a function that remove everything of "aux_string" until
+	// the first \n
+}
